4152d773e4b7d69470315a5cb648d33aa711e239
Amelie Soares <ameliejoaline.soares@gmail.com>
2026-03-17 12:40:20 +0200

    integrated cat and num preproc

---
+++ eda_package/registry.py
@@ -0,0 +1,9 @@
+ORDINAL_FEATURES_MAP = {
+    "arrival_date_month": [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ],
+    "meal": [
+        "Undefined", "SC", "BB", "HB", "FB"
+    ]
+}
